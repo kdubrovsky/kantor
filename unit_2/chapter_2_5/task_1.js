5eeb96f7829ea61a4b2e8f606ed0e926d6f9e9e4
Konstantin Tereschenkov <kdubrovsky@gmail.com>
2023-02-17 12:11:27 +0400

    Add chapter 2.5 task solution

---
+++ unit_2/chapter_2_5/task_1.js
@@ -0,0 +1,10 @@
+// Try to guess script output
+
+let name = "Ilya";
+
+console.log(`hello ${1}`); // hello 1
+
+console.log(`hello ${"name"}`); // hello name
+
+console.log(`hello ${name}`); // hello Ilya
+
